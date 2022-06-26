5508edbee9f4620f7b4484cfefac5ee081956bfb
joefrank03 <fernandezjoseph03@gmail.com>
2022-06-26 11:42:46 -0500

    ETL for Top Producer DB

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ import pandas as pd
 
 ## Load CVS File
 df = pd.read_csv('/Users/ferna/PycharmProjects/RemaxTopProducer/TopProducerDB.csv')
-print(df);
+#print(df);
 
 ## printing table
-print(df.to_string())
+## print(df.to_string())
 
+## deleting a column in a dataframe in python
+del df['Email Address Description']
+df.shape
+df.info()
 
 
