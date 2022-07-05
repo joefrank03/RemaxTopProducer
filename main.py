f797f30ef7583e35b204cd88b6e3bd2d628480ee
joefrank03 <fernandezjoseph03@gmail.com>
2022-07-05 18:35:09 -0500

    ETL for Top Producer DB

--- main.py
+++ main.py
@@ -2,18 +2,32 @@
 import numpy as np
 from sklearn.datasets import load_iris
 import pandas as pd
+import sys
+import os
 
 
-## Load CVS File
+# Load CVS File
 df = pd.read_csv('/Users/ferna/PycharmProjects/RemaxTopProducer/TopProducerDB.csv')
-#print(df);
+df.info()
+df.shape
+#printing table
+print(df.to_string())
 
-## printing table
-## print(df.to_string())
+
 
 ## deleting a column in a dataframe in python
-del df['Email Address Description']
-df.shape
-df.info()
+# df.drop(['Email Address Description'],axis=1, inplace=True)
+# print(df.to_string())
 
+# x = input("x: ")
+# # print(type(x))
+# y = int(x) + 1
+# print(f"x: {x}, y: {y}")
+
+# Load Remax Sales CSV file
+# how to find the file path using the command prompt
+# dir "\realEstateSales.csv/"s
+# realEstateSales = pd.read_csv('/Users/ferna/PycharmProjects/RemaxTopProducer/realEstateSales.csv')
+# print(realEstateSales.to_string())
+# realEstateSales.info()
 
